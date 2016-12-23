3a91b01d8aba3ed414ba729c49d115ca4b180435
antonmazun <kpi.study1@gmail.com>
2016-12-23 23:49:41 +0200

    bla

---
+++ lab/static/js/ajax_script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+        $(".deletePhone").click(function(){
+            btn = $(this);
+            inp = $("#delete-post");
+            inp.attr("value", btn.attr("value"));
+        });
+});
+
+
+$(document).ready(function(){
+        $("#delete-post").click(function(){
+            btn = $(this);
+            //alert(btn.attr("value"));
+            $.ajax({type: "delete", url: $(location).attr('pathname') + '?phone=' + btn.attr("value"), success: function(result){
+                alert(result);
+               
+                if (result.status === 'error'){
+                    alert('error');}
+                else
+                    {$("#phone_" + btn.attr("value")).remove();}
+            }});
+        });
+    });
